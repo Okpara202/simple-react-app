53abd9deb62b2d315e2c1638f4c42f8a747b1658
Okpara202 <okparafavour202@gmail.com>
2025-03-14 22:14:58 +0100

    done with settings stage

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { useAuth } from "../authentication/auth";
 import Header from "../components/header";
 import { Navigate } from "react-router-dom";
-import { FaSuitcase } from "react-icons/fa6";
+import { FaGears, FaSuitcase } from "react-icons/fa6";
+import { FaUserAstronaut } from "react-icons/fa";
 
 function Settings() {
   // Ensure that state persist after refresh
@@ -37,7 +38,7 @@ function Settings() {
           <h4>
             <span>
               <FaSuitcase />
-            </span>
+            </span>{" "}
             Jobs management
           </h4>
           <nav>
@@ -86,6 +87,122 @@ function Settings() {
             </aside>
             <aside>
               <input type="checkbox" name="" id="" />
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+            </aside>
+          </div>
+        </section>
+
+        <div className="job">
+          <h4>
+            <span>
+              <FaUserAstronaut />
+            </span>{" "}
+            Candidate management
+          </h4>
+          <nav>
+            <p>Intern</p>
+            <p>Associate</p>
+            <p>Manager</p>
+          </nav>
+        </div>
+        <section className="section-2">
+          <div>
+            <aside>
+              <p>Change candidate stage</p>
+            </aside>
+            <aside>
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+            </aside>
+          </div>
+
+          <div>
+            <aside>
+              <p>Move applicant to terminal stage (Hired, Rejected, on hold)</p>
+            </aside>
+            <aside>
+              <input type="checkbox" name="" id="" />
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+            </aside>
+          </div>
+
+          <div>
+            <aside>
+              <p>Read messages</p>
+            </aside>
+            <aside>
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+            </aside>
+          </div>
+
+          <div>
+            <aside>
+              <p>Send ad-hoc messages to candidates</p>
+            </aside>
+            <aside>
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+            </aside>
+          </div>
+
+          <div>
+            <aside>
+              <p>Reschedule interviews</p>
+            </aside>
+            <aside>
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+            </aside>
+          </div>
+          <div>
+            <aside>
+              <p>Import applicants</p>
+            </aside>
+            <aside>
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+            </aside>
+          </div>
+        </section>
+
+        <div className="job">
+          <h4>
+            <span>
+              <FaGears />
+            </span>{" "}
+            User management
+          </h4>
+          <nav>
+            <p>Intern</p>
+            <p>Associate</p>
+            <p>Manager</p>
+          </nav>
+        </div>
+        <section className="section-2">
+          <div>
+            <aside>
+              <p>Edit user details</p>
+            </aside>
+            <aside>
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+            </aside>
+          </div>
+          <div>
+            <aside>
+              <p>Participate interviews / Set calendar availability</p>
+            </aside>
+            <aside>
+              <input type="checkbox" name="" id="" defaultChecked />
               <input type="checkbox" name="" id="" defaultChecked />
               <input type="checkbox" name="" id="" defaultChecked />
             </aside>
