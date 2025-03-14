140c8bbcfcc558079c4cdc92d9e9998a5a254180
Okpara202 <okparafavour202@gmail.com>
2025-03-14 21:43:47 +0100

    working on settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useAuth } from "../authentication/auth";
 import Header from "../components/header";
 import { Navigate } from "react-router-dom";
+import { FaSuitcase } from "react-icons/fa6";
 
 function Settings() {
   // Ensure that state persist after refresh
@@ -18,6 +19,79 @@ function Settings() {
   return (
     <section>
       <Header user={userName!} role={userRole!} logout={logout} />
+
+      <main className="container settings">
+        <section className="section-1">
+          <aside>
+            <h3>Team members</h3>
+            <p className="gray">
+              Invite or manage your organisations's members.
+            </p>
+          </aside>
+          <aside>
+            <button>+ Add Member</button>
+          </aside>
+        </section>
+
+        <div className="job">
+          <h4>
+            <span>
+              <FaSuitcase />
+            </span>
+            Jobs management
+          </h4>
+          <nav>
+            <p>Intern</p>
+            <p>Associate</p>
+            <p>Manager</p>
+          </nav>
+        </div>
+        <section className="section-2">
+          <div>
+            <aside>
+              <p>Create new job and stages</p>
+            </aside>
+            <aside>
+              <input type="checkbox" name="" id="" />
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+            </aside>
+          </div>
+
+          <div>
+            <aside>
+              <p>Edit job and stages</p>
+            </aside>
+            <aside>
+              <input type="checkbox" name="" id="" />
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+            </aside>
+          </div>
+
+          <div>
+            <aside>
+              <p>Archive job</p>
+            </aside>
+            <aside>
+              <input type="checkbox" name="" id="" />
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+            </aside>
+          </div>
+
+          <div>
+            <aside>
+              <p>Change job status</p>
+            </aside>
+            <aside>
+              <input type="checkbox" name="" id="" />
+              <input type="checkbox" name="" id="" defaultChecked />
+              <input type="checkbox" name="" id="" defaultChecked />
+            </aside>
+          </div>
+        </section>
+      </main>
     </section>
   );
 }
