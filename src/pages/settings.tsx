58f76a28e56585733ae88d7962967b7399839626
Okpara202 <okparafavour202@gmail.com>
2025-03-16 10:00:48 +0100

    Converted settings to drop down

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -3,12 +3,21 @@ import { useAuth } from "../authentication/auth";
 import Header from "../components/header";
 import { Navigate } from "react-router-dom";
 import { FaGears, FaSuitcase } from "react-icons/fa6";
-import { FaUserAstronaut } from "react-icons/fa";
+import {
+  FaArrowAltCircleDown,
+  FaArrowAltCircleUp,
+  FaUserAstronaut,
+} from "react-icons/fa";
 
 function Settings() {
   // Ensure that state persist after refresh
   const [userName] = useState<string | null>(localStorage.getItem("name"));
   const [userRole] = useState<string | null>(localStorage.getItem("role"));
+
+  // For dropDown
+  const [job, setJob] = useState<boolean>(false);
+  const [candidate, setCandidate] = useState<boolean>(false);
+  const [user, setUser] = useState<boolean>(false);
 
   // Allow logging out
   const { logout } = useAuth();
@@ -34,12 +43,15 @@ function Settings() {
           </aside>
         </section>
 
-        <div className="job">
+        <div onClick={() => setJob((prevState) => !prevState)} className="job">
           <h4>
             <span>
               <FaSuitcase />
             </span>{" "}
             Jobs management
+            <span className="arrow">
+              {job ? <FaArrowAltCircleUp /> : <FaArrowAltCircleDown />}
+            </span>{" "}
           </h4>
           <nav>
             <p>Viewer</p>
@@ -47,58 +59,114 @@ function Settings() {
             <p>Admin</p>
           </nav>
         </div>
-        <section className="section-2">
-          <div>
-            <aside>
-              <p>Create new job and stages</p>
-            </aside>
-            <aside>
-              <input type="checkbox" name="" id="" />
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-            </aside>
-          </div>
+        {job && (
+          <section className="section-2">
+            <div>
+              <aside>
+                <p>Create new job and stages</p>
+              </aside>
+              <aside>
+                <input className="check1" type="checkbox" name="" id="" />
+                <input
+                  className="check2"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+              </aside>
+            </div>
 
-          <div>
-            <aside>
-              <p>Edit job and stages</p>
-            </aside>
-            <aside>
-              <input type="checkbox" name="" id="" />
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-            </aside>
-          </div>
+            <div>
+              <aside>
+                <p>Edit job and stages</p>
+              </aside>
+              <aside>
+                <input className="check1" type="checkbox" name="" id="" />
+                <input
+                  className="check2"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+              </aside>
+            </div>
 
-          <div>
-            <aside>
-              <p>Archive job</p>
-            </aside>
-            <aside>
-              <input type="checkbox" name="" id="" />
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-            </aside>
-          </div>
+            <div>
+              <aside>
+                <p>Archive job</p>
+              </aside>
+              <aside>
+                <input className="check1" type="checkbox" name="" id="" />
+                <input
+                  className="check2"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+              </aside>
+            </div>
 
-          <div>
-            <aside>
-              <p>Change job status</p>
-            </aside>
-            <aside>
-              <input type="checkbox" name="" id="" />
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-            </aside>
-          </div>
-        </section>
+            <div>
+              <aside>
+                <p>Change job status</p>
+              </aside>
+              <aside>
+                <input className="check1" type="checkbox" name="" id="" />
+                <input
+                  className="check2"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+              </aside>
+            </div>
+          </section>
+        )}
 
-        <div className="job">
+        <div
+          className="job"
+          onClick={() => setCandidate((prevState) => !prevState)}
+        >
           <h4>
             <span>
               <FaUserAstronaut />
             </span>{" "}
             Candidate management
+            <span className="arrow">
+              {candidate ? <FaArrowAltCircleUp /> : <FaArrowAltCircleDown />}
+            </span>
           </h4>
           <nav>
             <p>Viewer</p>
@@ -106,79 +174,188 @@ function Settings() {
             <p>Admin</p>
           </nav>
         </div>
-        <section className="section-2">
-          <div>
-            <aside>
-              <p>Change candidate stage</p>
-            </aside>
-            <aside>
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-            </aside>
-          </div>
+        {candidate && (
+          <section className="section-2">
+            <div>
+              <aside>
+                <p>Change candidate stage</p>
+              </aside>
+              <aside>
+                <input
+                  className="check1"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check2"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+              </aside>
+            </div>
 
-          <div>
-            <aside>
-              <p>Move applicant to terminal stage (Hired, Rejected, on hold)</p>
-            </aside>
-            <aside>
-              <input type="checkbox" name="" id="" />
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-            </aside>
-          </div>
+            <div>
+              <aside>
+                <p>
+                  Move applicant to terminal stage (Hired, Rejected, on hold)
+                </p>
+              </aside>
+              <aside>
+                <input className="check1" type="checkbox" name="" id="" />
+                <input
+                  className="check2"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+              </aside>
+            </div>
 
-          <div>
-            <aside>
-              <p>Read messages</p>
-            </aside>
-            <aside>
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-            </aside>
-          </div>
+            <div>
+              <aside>
+                <p>Read messages</p>
+              </aside>
+              <aside>
+                <input
+                  className="check1"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check2"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+              </aside>
+            </div>
 
-          <div>
-            <aside>
-              <p>Send ad-hoc messages to candidates</p>
-            </aside>
-            <aside>
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-            </aside>
-          </div>
+            <div>
+              <aside>
+                <p>Send ad-hoc messages to candidates</p>
+              </aside>
+              <aside>
+                <input
+                  className="check1"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check2"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+              </aside>
+            </div>
 
-          <div>
-            <aside>
-              <p>Reschedule interviews</p>
-            </aside>
-            <aside>
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-            </aside>
-          </div>
-          <div>
-            <aside>
-              <p>Import applicants</p>
-            </aside>
-            <aside>
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-            </aside>
-          </div>
-        </section>
+            <div>
+              <aside>
+                <p>Reschedule interviews</p>
+              </aside>
+              <aside>
+                <input
+                  className="check1"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check2"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+              </aside>
+            </div>
+            <div>
+              <aside>
+                <p>Import applicants</p>
+              </aside>
+              <aside>
+                <input
+                  className="check1"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check2"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+              </aside>
+            </div>
+          </section>
+        )}
 
-        <div className="job">
+        <div className="job" onClick={() => setUser((prevState) => !prevState)}>
           <h4>
             <span>
               <FaGears />
             </span>{" "}
             User management
+            <span className="arrow">
+              {user ? <FaArrowAltCircleUp /> : <FaArrowAltCircleDown />}
+            </span>{" "}
           </h4>
           <nav>
             <p>Viewer</p>
@@ -186,28 +363,66 @@ function Settings() {
             <p>Admin</p>
           </nav>
         </div>
-        <section className="section-2">
-          <div>
-            <aside>
-              <p>Edit user details</p>
-            </aside>
-            <aside>
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-            </aside>
-          </div>
-          <div>
-            <aside>
-              <p>Participate interviews / Set calendar availability</p>
-            </aside>
-            <aside>
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-              <input type="checkbox" name="" id="" defaultChecked />
-            </aside>
-          </div>
-        </section>
+        {user && (
+          <section className="section-2">
+            <div>
+              <aside>
+                <p>Edit user details</p>
+              </aside>
+              <aside>
+                <input
+                  className="check1"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check2"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+              </aside>
+            </div>
+            <div>
+              <aside>
+                <p>Participate interviews / Set calendar availability</p>
+              </aside>
+              <aside>
+                <input
+                  className="check1"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check2"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+                <input
+                  className="check3"
+                  type="checkbox"
+                  name=""
+                  id=""
+                  defaultChecked
+                />
+              </aside>
+            </div>
+          </section>
+        )}
       </main>
     </section>
   );
